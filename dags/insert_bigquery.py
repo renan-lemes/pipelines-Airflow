2d864fe0738ba9254f6449468310dd5099ecae47
RenanLemes <renan_ll@hotmail.com>
2025-06-15 20:36:18 -0300

    Chores:atualizando o insert no bigquery

--- dags/insert_bigquery.py
+++ dags/insert_bigquery.py
@@ -1,11 +1,15 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-import mysql.connector
 from google.cloud import bigquery
+from google.api_core.exceptions import GoogleAPIError
 from cred.load_cred import load_creds_bq, load_creds_mysql
-import os
 from dotenv import load_dotenv
+import mysql.connector
+import os   
+import re
+import pandas as pd
+
 
 load_dotenv(dotenv_path='/opt/airflow/dags/.env')
 
@@ -15,7 +19,7 @@ creds_mysql = load_creds_mysql()
 
 # Configuração de conexão
 MYSQL_CONN = {
-    'host': os.getenv('HOST_MYSQL'),
+    'host': 'host.docker.internal',
     'user': os.getenv('USERNAME_MYSQL'),
     'password': os.getenv('PASSWORD_MYSQL'),
     'database': os.getenv('MYSQL_DATABASE'),
@@ -28,6 +32,9 @@ BQ_TABLE = 'customers_from_mysql'
 table_origem_mysql = 'olist_customers_dataset'
 
 table = 'warehouse'
+
+def sanitize_column(name):
+    return re.sub(r'[^a-zA-Z0-9_]', '_', name)
 
 # Extração do MySQL
 def extract_from_mysql(**kwargs):
@@ -46,39 +53,66 @@ def extract_from_mysql(**kwargs):
         conn.close()
     return rows
 
-# Criação da tabela no BigQuery
+# Criação da tabela no BigQuery caso não exista
 def create_bq_table_if_not_exists(client, dataset_id, table_id, rows):
+    if not rows:
+        raise ValueError("Nenhuma linha retornada do MySQL para criação da tabela no BigQuery.")
+
     dataset_ref = client.dataset(dataset_id)
     table_ref = dataset_ref.table(table_id)
 
     try:
         client.get_table(table_ref)
-    except Exception:
-        sample_row = rows[0]
-        schema = [bigquery.SchemaField(name, "STRING") for name in sample_row.keys()]
-        table = bigquery.Table(table_ref, schema=schema)
-        client.create_table(table)
+        print(f"Tabela {dataset_id}.{table_id} já existe no BigQuery.")
+    except GoogleAPIError as e:
+        print(f"Tabela {dataset_id}.{table_id} não encontrada. Será criada.")
 
+        # Usa a primeira linha para inferir o schema
+        sample_row = rows[0]
+
+        schema = [
+            bigquery.SchemaField(sanitize_column(name), "STRING")
+            for name in sample_row.keys()
+        ]
+
+        # Criação da tabela com schema sanitizado
+        table = bigquery.Table(table_ref, schema=schema)
+
+        try:
+            client.create_table(table)
+            print(f"Tabela {dataset_id}.{table_id} criada com sucesso no BigQuery.")
+        except GoogleAPIError as create_err:
+            raise Exception(f"Erro ao criar tabela no BigQuery: {create_err.message}")
+        
 # Carregamento no BigQuery
 def load_to_bigquery(**kwargs):
     rows = kwargs['ti'].xcom_pull(task_ids='extract_mysql')
     if not rows:
+        print("Nenhum dado para inserir no BigQuery.")
         return
 
+    df = pd.DataFrame(rows)
+
+    # transformando as colunas em str
+    cols = df.columns
+    for col in cols:
+        df[col] = df[col].astype(str)
+
     client = bigquery.Client()
-    create_bq_table_if_not_exists(client, BQ_DATASET, BQ_TABLE, rows)
-    table_ref = f"{BQ_PROJECT_ID}.{BQ_DATASET}.{BQ_TABLE}"
-    errors = client.insert_rows_json(table_ref, rows)
-    if errors:
-        raise Exception(f"Erro ao inserir no BigQuery: {errors}")
+    table_id = f"{BQ_PROJECT_ID}.{BQ_DATASET}.{BQ_TABLE}"
+
+    job = client.load_table_from_dataframe(df, table_id)
+    job.result()  # Aguarda a conclusão
+
+    print(f"{job.output_rows} linhas inseridas com sucesso no BigQuery.")
 
 # DAG
 with DAG(
     dag_id='mysql_to_bigquery_only',
     start_date=datetime(2024, 1, 1),
-    schedule_interval='@daily',
+    schedule_interval=None, ## aqui posso adicionar se e diario ou a forma que preferir exemplo '0 7-17 * * 1-5'
     catchup=False,
-    tags=['mysql', 'bigquery', 'etl']
+    tags=['mysql', 'bigquery', 'raw']
 ) as dag:
 
     extract_mysql = PythonOperator(
